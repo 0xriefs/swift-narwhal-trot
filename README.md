74add5a81b01fc4f47e7c6b2ba3567a8c3e034b5
[dyad] <arif.sukmawan02@gmail.com>
2025-09-17 15:35:38 +0700

    Reverted all changes back to version 5b1cf7f9e6b1302b5b24e7ecaf026116d0d02355

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-## TEFA TM
-
-This app was created using https://getmocha.com.
-Need help or want to join the community? Join our [Discord](https://discord.gg/shDEGBSe2d).
-
-To run the devserver:
-```
-npm install
-npm run dev
-```
+# Welcome to your Dyad app
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import { mochaPlugins } from "@getmocha/vite-plugins";
+import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
+export default defineConfig(() => ({
   server: {
-    allowedHosts: true,
+    host: "::",
+    port: 8080,
   },
-  build: {
-    chunkSizeWarningLimit: 5000,
-  },
+  plugins: [dyadComponentTagger(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
